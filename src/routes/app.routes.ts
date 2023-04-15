ea19f7c96c2c43f2a08e21170500589133225da5
feliperx <feliipe.xaud@gmail.com>
2023-04-14 22:43:38 -0400

    added routes

---
+++ src/routes/app.routes.ts
@@ -0,0 +1,9 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+
+const { Navigator, Screen } = createStackNavigator();
+
+export function AppRoutes() {
+  return 
+}
