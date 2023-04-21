83d0e496117f83776be15730ccda482ab7009a88
feliperx <feliipe.xaud@gmail.com>
2023-04-21 01:23:39 -0400

    added GradientView component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { Text, TouchableOpacity, TouchableOpacityProps } from "react-native";
+import {
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  View,
+} from "react-native";
 import { SvgProps } from "react-native-svg";
 import { LinearGradient } from "expo-linear-gradient";
 
 import { styles } from "./styles";
 import theme from "../../global/styles/theme";
+import { GradientView } from "../GradientView";
 
 type Props = TouchableOpacityProps & {
   title: string;
@@ -16,13 +22,12 @@ export function Button({ title, icon: Icon, ...rest }: Props) {
 
   return (
     <TouchableOpacity activeOpacity={0.7} {...rest}>
-      <LinearGradient
-        style={styles.container}
-        colors={[primary, highlight]}
-      >
-        {Icon && <Icon height={22} width={22}/>}
-        <Text style={[styles.title, Icon && { marginLeft:8}]}>{title}</Text>
-      </LinearGradient>
+      <GradientView>
+        <View style={styles.container}>
+          {Icon && <Icon height={22} width={22} />}
+          <Text style={[styles.title, Icon && { marginLeft: 8 }]}>{title}</Text>
+        </View>
+      </GradientView>
     </TouchableOpacity>
   );
 }
---
+++ src/components/GradientView/index.tsx
@@ -0,0 +1,24 @@
+import React, { ReactNode } from "react";
+import { View } from "react-native";
+
+import { styles } from "./styles";
+import theme from "../../global/styles/theme";
+import { LinearGradient } from "expo-linear-gradient";
+
+type Props = {
+  children: ReactNode;
+};
+
+export function GradientView({ children, ...rest }: Props) {
+  const { primary, highlight } = theme.colors;
+
+  return (
+    <LinearGradient
+      style={styles.container}
+      colors={[primary, highlight]}
+      {...rest}
+    >
+      {children}
+    </LinearGradient>
+  );
+}
