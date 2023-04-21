25ebbf94f4771e47177751873714a71bfb2f0af4
feliperx <feliipe.xaud@gmail.com>
2023-04-21 01:09:56 -0400

    added Header component

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { useFonts } from "expo-font";
 import {
   Poppins_400Regular,
-  // Poppins_500Medium,
+  Poppins_500Medium,
   Poppins_600SemiBold,
   Poppins_700Bold,
 } from "@expo-google-fonts/poppins";
@@ -14,12 +14,13 @@ import {
 import AppLoading from "expo-app-loading";
 
 import { Routes } from "./src/routes";
+import { SignIn } from "./src/screens/SignIn";
 
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
-    // Poppins_500Medium,
+    Poppins_500Medium,
     Poppins_600SemiBold,
     Poppins_700Bold,
     Roboto_400Regular,
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,18 +1,16 @@
 import React from "react";
-import {
-  KeyboardAvoidingView,
-  ScrollView,
-  Text,
-  View,
-} from "react-native";
+import { KeyboardAvoidingView, ScrollView, Text, View } from "react-native";
 
 import { styles } from "./styles";
-
+import { Header } from "../../components/Header";
 
 export function Home() {
   return (
-        <View style={styles.container}>
-          <Text>Home</Text>
-        </View>
+    <View style={styles.container}>
+      <Header title="Visao Geral" />
+      <View style={styles.content}>
+        <Text>Home</Text>
+      </View>
+    </View>
   );
 }
